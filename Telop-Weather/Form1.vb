2abce3c4a2c4cbdc6b3c171620d32c610704354b
cope731 <cope73123@gmail.com>
2016-12-10 11:22:36 +0900

    Ver.1.0

---
+++ Telop-Weather/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Point1.Text = "こんにちは""
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Point1.Click
+
+    End Sub
+End Class
